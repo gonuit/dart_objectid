d5f04081964506cdebc3fe3bcfeecf90109516c5
Kamil Klyta <kamilklyta341@gmail.com>
2020-09-03 22:06:50 +0200

    Add example console app

---
+++ example/bin/example.dart
@@ -0,0 +1,5 @@
+import 'package:objectid/objectid.dart';
+
+void main(List<String> arguments) {
+  print('ObjectId example: ${ObjectId()}!');
+}
--- lib/objectid.dart
+++ lib/objectid.dart
@@ -1,2 +1,8 @@
 library objectid;
 
+class ObjectId {
+  String _value;
+  ObjectId() {
+    _value = "Generated objectId";
+  }
+}
